dc77f4c3e910e0c38c5889c90f1c1aab803326ff
Your Name <your.email@example.com>
2024-08-23 13:45:30 +0700

    new web

--- js/script.js
+++ js/script.js
@@ -56,15 +56,17 @@ function toggleMenu() {
     nav.classList.toggle('expanded');
 }
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
     });
-});
 
-const hiddenElement = document.querySelectorAll('.hidden');
-hiddenElement.forEach((el) => observer.observe(el));
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+});
